b079358593e49fd36c924fd7fe76f79dc7fdb90f
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-04-23 22:50:58 -0300

    Update Main.java

--- src/ejer4/Main.java
+++ src/ejer4/Main.java
@@ -4,31 +4,6 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
    Scanner teclado = new Scanner(System.in);
-        int[] numero = new int[5];
-        int i=0;
-        int mayor = numero[0];
-                                                     
-        System.out.println("Ingrese 5 números positivos: ");
-      	 do {
-            System.out.print("numero[" + i + "]= ");
-            numero[i] = teclado.nextInt();
-            if (numero[i] < 0) {
-            	System.out.println("Error");  
-        }
-            else {
-            	i++;   	
-        }
-      	 }while(i<5);
    
-        System.out.println("Positivos: " + i);     
-        		
-		for ( i = 0; i < 5; i++) {
-			if (numero[i] > mayor) {
-				 mayor = numero[i];
-			}
-		}
-		System.out.println("El mayor es: " + mayor);
-        
-        
   }
 }
