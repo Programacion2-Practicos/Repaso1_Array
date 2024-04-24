673b54aa1aa0452cda4e59eca0c434bda48f9414
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-04-23 22:50:10 -0300

    Update Main.java

--- src/ejer3/Main.java
+++ src/ejer3/Main.java
@@ -5,33 +5,7 @@ class Main {
   public static void main(String[] args) {
 		Scanner teclado=new Scanner(System.in);
 		
-		double promedio;
-		int valor;
-		int suma=0;
-		int i=0;
-	
-		System.out.println("Ingrese las edades:");	
-		int[] edades = new int[10];
-	
 		
-	
-		do {
-			valor = teclado.nextInt();
-			if (valor <18) {
-				System.out.println("Error, las personas deben ser mayores de edad, ingrese de nuevo");
-				
-			}
-			else {
-				
-				edades [i] = valor;
-				suma=(suma+valor);
-				i++;
-			}
-		}while(i <10);
-		
-		
-		promedio =  suma/i;
-		System.out.print("Promedio de edades ingresadas: " + promedio);
 
 	}
 }
