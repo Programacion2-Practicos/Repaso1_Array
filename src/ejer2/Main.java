b45d68a0e3903a72ebecc742ea14a932c4a82d50
Elizabeth Izquierdo <73123352+eliizquierdo@users.noreply.github.com>
2024-04-23 22:49:26 -0300

    Update Main.java

--- src/ejer2/Main.java
+++ src/ejer2/Main.java
@@ -4,20 +4,6 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
    Scanner teclado=new Scanner(System.in);
-		int[] sueldos = new int[6];
-		int acumulador = 0 ;
-		double promedio=0;
-		int suma = 0;
-		for (int i = 0; i < 6; i++) {
-      System.out.print("Ingrese 6 sueldos: "+(i+1)+": ");
-			sueldos[i] = teclado.nextInt();	
-			if (sueldos[i] > 5000) {
-				acumulador++;
-				 suma = (suma + sueldos[i]);
-			} 
-		}
-    if (acumulador!=0)
-		  promedio = suma / acumulador;
-		System.out.print("Promedio de los sueldos ingresadas mayores a $5000: " + promedio);
-		}
+	
+}
   }
